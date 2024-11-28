69fd85b0c90ced8a1e5a5e7eeec1244e80129fb5
SirAlexus1811 <gummi.hubi@gmail.com>
2024-11-28 22:31:13 +0100

    New vscode Compiler tasks and testprogram added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+#include <glad/glad.h> //Must be included before glfw3.h
+#include <GLFW/glfw3.h>
+
 int main() {
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     return 0;
 }
